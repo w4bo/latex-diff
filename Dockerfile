c9c439efb1a8d92311d156e6fa4d9bd2348c4d46
mfrancia <matteo.francia2@studio.unibo.it>
2022-02-11 08:35:55 +0100

    update latexdiff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,2 @@
 FROM blang/latex
 RUN apt install -y latexdiff
-COPY diff.sh /data/diff.sh
-RUN chmod +x /data/diff.sh
--- diff.sh
+++ diff.sh
@@ -1,4 +1,6 @@
-cp -R /.ssh ~/.ssh
+cd /data
+
+cp -R .ssh ~/.ssh
 chmod 400 ~/.ssh/id_rsa
 
 if [ -f .env ]
@@ -6,26 +8,26 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-cd /data
-
-rm -rf old
-git clone $REPO old
-cd /data/old
+git clone $REPO /old
+cd /old
 git checkout $COMMIT
 
 cd -
-rm -rf new
-git clone $REPO new
+git clone $REPO /new
 
 rm -rf diff
 mkdir diff
-cp -R new/* diff
+cp -R /new/* diff
 
-latexdiff --preamble=/data/preamble.tex --flatten old/main.tex new/main.tex > diff/diff.tex
-latexdiff --preamble=/data/preamble.tex old/references.bib new/references.bib > diff/references.bib
+latexdiff --preamble=/data/preamble.tex --flatten /old/main.tex /new/main.tex > diff/diff.tex
+# latexdiff /old/references.bib /new/references.bib > diff/references.bib
 
-latexdiff --flatten old/main.tex new/main.tex > diff/diff-all.tex
-latexdiff old/references.bib new/references.bib > diff/references.bib
+rm -rf diff-all
+mkdir diff-all
+cp -R new/* diff-all
+
+latexdiff --flatten /old/main.tex /new/main.tex > diff-all/diff-all.tex
+# latexdiff /old/references.bib /new/references.bib > diff-all/references.bib
 
 cd /data/diff
 
@@ -33,8 +35,12 @@ pdflatex -file-line-error -interaction=batchmode diff.tex || true
 bibtex diff.aux || true
 pdflatex -file-line-error -interaction=batchmode diff.tex || true
 pdflatex -file-line-error -interaction=batchmode diff.tex || true
+rm -rf *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl *.out *.blg *.spl
+
+cd /data/diff-all
 
 pdflatex -file-line-error -interaction=batchmode diff-all.tex || true
 bibtex diff-all.aux || true
 pdflatex -file-line-error -interaction=batchmode diff-all.tex || true
 pdflatex -file-line-error -interaction=batchmode diff-all.tex || true
+rm -rf *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl *.out *.blg *.spl
