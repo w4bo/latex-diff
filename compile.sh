f3b0b6c3ec0f24428b2ad6a4bf053cd2bf901b19
Matteo Francia <m.francia@unibo.it>
2022-12-20 15:11:29 +0100

    update the scripts

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -exo
 lacheck $1.tex >> $1.lacheck.txt
-pdflatex -file-line-error -interaction=batchmode $1.tex || true
-bibtex $1.aux || true
-pdflatex -file-line-error -interaction=batchmode $1.tex || true
-pdflatex -file-line-error -interaction=batchmode $1.tex || true
-rm -rf *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl *.out *.blg *.spl
+latexmk -f -pdf -interaction=nonstopmode $1.tex || true
+# pdflatex -file-line-error -interaction=batchmode $1.tex || true
+# bibtex $1 || true
+# pdflatex -file-line-error -interaction=batchmode $1.tex || true
+# pdflatex -file-line-error -interaction=batchmode $1.tex || true
+# rm -rf *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl *.out *.blg *.spl
--- diff.sh
+++ diff.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -exo
 cd /data
 
 chmod +x *.sh
@@ -8,7 +10,6 @@ chmod 400 ~/.ssh/id_rsa
 rm -rf generated
 mkdir generated
 
-
 if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
@@ -18,29 +19,30 @@ rm -rf /old
 git clone $REPO /old
 cd /old
 git checkout $COMMIT
-sh /data/compile.sh main
-cp main.pdf /data/generated/main-$COMMIT.pdf
-# cp main.lacheck.txt /data/generated/main-$COMMIT.lacheck.txt
+sh /data/compile.sh $MAINFILE
+cp $MAINFILE.pdf /data/generated/$MAINFILE-$COMMIT.pdf
+# cp $MAINFILE.lacheck.txt /data/generated/$MAINFILE-$COMMIT.lacheck.txt
 
 cd -
 rm -rf /new
 git clone $REPO /new
 cd /new
-sh /data/compile.sh main
-cp main.lacheck.txt /data/generated/main.lacheck.txt
+sh /data/compile.sh $MAINFILE
+cp $MAINFILE.lacheck.txt /data/generated/$MAINFILE.lacheck.txt
+cp $MAINFILE.pdf /data/generated/
 
 rm -rf /diff
 mkdir /diff
 cp -R /new/* /diff
 
-latexdiff --preamble=/data/preamble.tex --flatten /old/main.tex /new/main.tex > /diff/diff.tex
+latexdiff --preamble=/data/preamble.tex --flatten /old/$MAINFILE.tex /new/$MAINFILE.tex > /diff/diff.tex
 # latexdiff /old/references.bib /new/references.bib > /diff/references.bib
 
 rm -rf /diff-all
 mkdir /diff-all
 cp -R /new/* /diff-all
 
-latexdiff --flatten /old/main.tex /new/main.tex > /diff-all/diff-all.tex
+latexdiff --flatten /old/$MAINFILE.tex /new/$MAINFILE.tex > /diff-all/diff-all.tex
 # latexdiff /old/references.bib /new/references.bib > /diff-all/references.bib
 
 cd /diff
